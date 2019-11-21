a071be90fd3ff5cf1e1aba8b32884dedd9ba2215
guzhikova <guzhikova.a@gmail.com>
2019-11-22 01:42:08 +0400

    Начат 1.7

---
+++ GuzhikovaTasks/Task1_Language/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Language
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------Task 1.7. ARRAY PROCESSING----------------------");
+            ArrayProcessing();
+
+            Console.ReadKey();
+        }
+        
+        static void ArrayProcessing()
+        {
+            int[] array = CreateArray(20);
+            Console.WriteLine(Environment.NewLine + "Исходный массив:");
+            DisplayArray(array);
+
+        }
+        static int[] CreateArray(int length)
+        {
+            int[] array = new int[length];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100);
+            }
+
+            return array;
+        }
+
+        static void DisplayArray(int[] array)
+        {
+            Console.Write(Environment.NewLine + "{");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]} ");
+            }
+
+            Console.Write("}" + Environment.NewLine);
+        }
+    }
+}
