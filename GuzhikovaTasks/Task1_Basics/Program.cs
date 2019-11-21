053f54ddd38fe9c66214e18a976d19a90b60374f
Guzhikova <guzhikova.a@gmail.com>
2019-11-21 12:22:51 +0400

    Добавлен Task 1.5

--- GuzhikovaTasks/Task1_Basics/Program.cs
+++ GuzhikovaTasks/Task1_Basics/Program.cs
@@ -18,20 +18,21 @@ namespace Task1_Basics
 
             Console.WriteLine("\n----------------------Task 1.3. ANOTHER TRIANGLE----------------------");
             Console.Write("Введите число: ");
-            DrawIsoscelesTriangle(ReadNumberFromConsole());            
+            DrawIsoscelesTriangle(ReadNumberFromConsole());
 
             Console.WriteLine("\n----------------------Task 1.4. X-MAS TREE----------------------");
             DrawTree();
 
+            Console.WriteLine("\n----------------------1.5. SUM OF NUMBERS----------------------");
+            SumOfNumbers();
+
             Console.ReadKey();
         }
 
-        static int ReadNumberFromConsole ()
+        static int ReadNumberFromConsole()
         {
             int number;
             string enteredString = Console.ReadLine();
-
-            // УЧЕСТЬ ИГНОР и ДОПИСАТЬ
 
             while (!(Int32.TryParse(enteredString, out number) && number > 0))
             {
@@ -45,8 +46,8 @@ namespace Task1_Basics
             return number;
         }
 
-        static void RectangleArea ()
-        { 
+        static void RectangleArea()
+        {
             Console.Write("Введите сторону а: ");
             int a = ReadNumberFromConsole();
 
@@ -66,23 +67,23 @@ namespace Task1_Basics
             StringBuilder stars = new StringBuilder(number);
 
             for (int i = 0; i < number; i++)
-            {                
+            {
                 Console.WriteLine(stars.Append("*"));
             }
         }
-        
-        
+
+
         /// <param name="height">The height of triangle is equal to the number of lines</param>
         /// <param name="shift">The left margin</param>
         static void DrawIsoscelesTriangle(int height, int shift = 0)
         {
-           
+
             StringBuilder stars = new StringBuilder(shift + (height * 2 - 1));
             stars.Append(' ', shift + (height * 2 - 1));
 
             for (int index = (height - 1); index >= 0; index--)
             {
-                stars.Insert(shift + index, "*", (index  == height - 1) ? 1 : 2);
+                stars.Insert(shift + index, "*", (index == height - 1) ? 1 : 2);
 
                 Console.WriteLine(stars.ToString());
 
@@ -93,7 +94,7 @@ namespace Task1_Basics
             }
         }
 
-        static void DrawTree ()
+        static void DrawTree()
         {
             Console.Write("Введите число: ");
             int numberOfTriangles = ReadNumberFromConsole();
@@ -101,8 +102,24 @@ namespace Task1_Basics
             for (int height = 1; height <= numberOfTriangles; height++)
             {
                 DrawIsoscelesTriangle(height, numberOfTriangles - height);
-            }   
+            }
 
         }
+
+        static void SumOfNumbers()
+        {
+            int counter = 0;
+
+            for (int number = 1; number < 1000; number++)
+            {
+                if (number % 3 == 0 || number % 5 == 0)
+                {
+                    counter += number;
+                }
+            }
+            Console.WriteLine(Environment.NewLine + $"Сумма всех чисел меньше 1000, кратных 3 или 5 равна: {counter}");
+        }
     }
+
 }
+
