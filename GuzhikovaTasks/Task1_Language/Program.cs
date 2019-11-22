c1f7649a7560b21435ffcb36e899ff34a471fc9f
guzhikova <guzhikova.a@gmail.com>
2019-11-23 01:19:07 +0400

    Добавлен Task 1.8

--- GuzhikovaTasks/Task1_Language/Program.cs
+++ GuzhikovaTasks/Task1_Language/Program.cs
@@ -10,8 +10,11 @@ namespace Task1_Language
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----------------------Task 1.7. ARRAY PROCESSING----------------------");
+            Console.WriteLine($"----------------------Task 1.7. ARRAY PROCESSING----------------------{Environment.NewLine}");
             ArrayProcessing();
+
+            Console.WriteLine($"{Environment.NewLine}----------------------Task 1.8. NO POSITIVE----------------------{Environment.NewLine}");
+            NoPositive();
 
             Console.ReadKey();
         }
@@ -20,16 +23,15 @@ namespace Task1_Language
         {
             int[] array = CreateArray(20);
 
-            Console.Write($"{Environment.NewLine} Исходный массив: min = { MinValue(array)}, max = { MaxValue(array) + Environment.NewLine}");
+            Console.Write($"{Environment.NewLine} Исходный массив: min = {ArrayProcessing_MinValue(array)}, max = {ArrayProcessing_MaxValue(array) + Environment.NewLine}");
             DisplayArray(array);
 
-            SortArray(array);
+            ArrayProcessing_SortArray(array);
 
             Console.Write($"{Environment.NewLine} Отсортированный массив: {Environment.NewLine}");
             DisplayArray(array);
         }
-
-        static int MinValue(int[] array)
+        static int ArrayProcessing_MinValue(int[] array)
         {
             int min = array[0];
 
@@ -41,8 +43,7 @@ namespace Task1_Language
 
             return min;
         }
-
-        static int MaxValue(int[] array)
+        static int ArrayProcessing_MaxValue(int[] array)
         {
             int max = array[0];
 
@@ -54,34 +55,7 @@ namespace Task1_Language
 
             return max;
         }
-
-        static int[] CreateArray(int length)
-        {
-            int[] array = new int[length];
-
-            Random rnd = new Random();
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = rnd.Next(100);
-            }
-
-            return array;
-        }
-
-        static void DisplayArray(int[] array)
-        {
-            Console.Write(Environment.NewLine + "{");
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($" {array[i]} ");
-            }
-
-            Console.Write("}" + Environment.NewLine);
-        }
-
-        static void SortArray(int[] array)
+        static void ArrayProcessing_SortArray(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
@@ -98,5 +72,96 @@ namespace Task1_Language
                 array[min] = temp;
             }
         }
+
+        static int[] CreateArray(int length)
+        {
+            int[] array = new int[length];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100);
+            }
+
+            return array;
+        }
+        static void DisplayArray(int[] array)
+        {
+            Console.Write(Environment.NewLine + "{");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]} ");
+            }
+
+            Console.Write("}" + Environment.NewLine);
+        }
+
+
+        static void NoPositive()
+        {
+            int[ , , ] array = new int[2, 3, 5];
+
+            NoPositive_CreateArray(array);
+
+            Console.WriteLine(Environment.NewLine + "Исходный массив: ");
+            NoPositive_DisplayArray(array);
+
+            NoPositive_PositiveToZero(array);
+            Console.WriteLine(Environment.NewLine + "Обновленный массив: ");
+            NoPositive_DisplayArray(array);
+        }
+        static void NoPositive_CreateArray(int[ , , ] array)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        array[i, j, k] = rnd.Next(-100, 100);
+                    }
+                }
+            }
+        }
+        static void NoPositive_DisplayArray(int[ , , ] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine("{ ");
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("   { ");
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        Console.Write(array[i, j, k] + (k== (array.GetLength(2)-1) ? " " : ", "));
+                    }
+
+                    Console.Write((j == (array.GetLength(1) - 1) ? " } " : "},") + Environment.NewLine);
+                }
+
+                Console.WriteLine("}" + Environment.NewLine);
+            }
+        }
+        static void NoPositive_PositiveToZero(int[,,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(2); k++)
+                    {
+                        if (array[i, j, k] >= 0)
+                            array[i, j, k] = 0;
+                    }
+                }
+            }
+        }
+
+
     }
 }
