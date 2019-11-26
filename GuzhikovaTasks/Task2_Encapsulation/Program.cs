2603b39ec3f5cd7cf92e89c7d20d1fe9f0ef1804
guzhikova <guzhikova.a@gmail.com>
2019-11-26 13:32:15 +0400

    Task 2 - пока пустой

---
+++ GuzhikovaTasks/Task2_Encapsulation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+
+    class Round
+    {
+        public Round()
+        {
+            _centerX = 0;
+            _centerY = 0;
+            _radius = 1;
+        }
+
+        public Round(double x, double y, double r)
+        {
+
+        }
+
+        private double _centerX, _centerY, _radius;
+        public int Length { get; set; }
+        public int Area { get; set; }
+
+    }
+}
