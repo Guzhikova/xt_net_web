4271b6747cce3a03b15ce8814f80a555a4c72070
guzhikova <guzhikova.a@gmail.com>
2019-11-27 14:49:26 +0400

    Сделан Task 2.2

--- GuzhikovaTasks/Task2_Encapsulation/Program.cs
+++ GuzhikovaTasks/Task2_Encapsulation/Program.cs
@@ -10,10 +10,11 @@ namespace Task2_Encapsulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"----------------------Task 2.1. ROUND----------------------{Environment.NewLine}");
-            Round();
+            //Console.WriteLine($"----------------------Task 2.1. ROUND----------------------{Environment.NewLine}");
+            //Round();
 
-            Console.WriteLine($"{Environment.NewLine}----------------------Task 2.2. TRIANGLE----------------------{Environment.NewLine}");
+            Console.WriteLine($"{Environment.NewLine}----------------------Task 2.2. TRIANGLE----------------------");
+            Triangle();
 
             Console.ReadKey();
         }
@@ -70,7 +71,7 @@ namespace Task2_Encapsulation
         static void Round_Change(Round round)
         {
             int x, y, radius;
-            Round_GetParams(out x, out y, out radius);
+            GetParameters("X", "Y", "Radius", out x, out y, out radius);
 
             round.X = x;
             round.Y = y;
@@ -82,62 +83,152 @@ namespace Task2_Encapsulation
         static void Round_CreateNew()
         {
             int x, y, radius;
-            Round_GetParams(out x, out y, out radius);
+            GetParameters("X", "Y", "Radius", out x, out y, out radius);
 
             Round round = new Round(x, y, radius);
 
             Console.WriteLine($"{Environment.NewLine}---ЗАДАН НОВЫЙ КРУГ: ");
             Round_GetInfo(round);
         }
-        static void Round_GetParams(out int x, out int y, out int radius)
+        //static void Round_GetParams(out int x, out int y, out int radius)
+        //{
+        //    Console.Write($"{Environment.NewLine}Введите координаты центра и радиус для круга: {Environment.NewLine}X = ");
+        //    x = ReadNumberFromConsole();
+        //    Console.Write("Y = ");
+        //    y = ReadNumberFromConsole();
+        //    Console.Write("r = ");
+        //    radius = ReadNumberFromConsole();
+        //}
+
+        /// <summary>
+        /// The method reads three int parameters from Console
+        /// </summary>
+        /// <param name="name1">First parameters name</param>
+        /// <param name="name2">Second parameters name</param>
+        /// <param name="name3">Third parameters name</param>
+        /// <param name="param1">First parameter</param>
+        /// <param name="param2">Second parameter</param>
+        /// <param name="param3">Third parameter</param>
+        static void GetParameters(string name1, string name2, string name3, out int param1, out int param2, out int param3)
         {
-            Console.Write($"{Environment.NewLine}Введите координаты центра и радиус для круга: {Environment.NewLine}X = ");
-            x = ReadNumberFromConsole();
-            Console.Write("Y = ");
-            y = ReadNumberFromConsole();
-            Console.Write("r = ");
-            radius = ReadNumberFromConsole();
+            Console.Write($"{Environment.NewLine}Введите данные: {Environment.NewLine}{name1} = ");
+            param1 = ReadNumberFromConsole();
+            Console.Write($"{name2} = ");
+            param2 = ReadNumberFromConsole();
+            Console.Write($"{name3} = ");
+            param3 = ReadNumberFromConsole();
         }
 
+        static void Triangle()
+        {
+            Triangle triangle = new Triangle();
+            int a = 0, b = 0, c = 0;
+            bool isCorrectSides;
+
+            do
+            {
+                try
+                {
+                    GetParameters("A", "B", "C", out a, out b, out c);
+                    triangle = new Triangle(a, b, c);
+                    isCorrectSides = true;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    isCorrectSides = false;
+                }
+                catch (ArithmeticException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    isCorrectSides = false;
+                }
+            } while (!isCorrectSides);
+
+            Console.WriteLine($"Периметр данного треугольника равен {triangle.GetPerimeter()}, площадь равна {triangle.GetArea().ToString("0.00")}");
+        }
+
+
+    }
+    class Round
+    {
+        public Round()
+        {
+            X = 0;
+            Y = 0;
+            Radius = 1;
+        }
+
+        public Round(int x, int y, int radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        private int _radius;
+        public int Radius
+        {
+            get { return _radius; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Radius should be more than zero", nameof(value));
+
+                _radius = value;
+            }
+        }
+
+        public double Length => 2 * Math.PI * Radius;
+        public double Area => Math.PI * Radius * Radius;
+
+    }
+
+    class Triangle
+    {
+
+        public Triangle()
+        {
+            A = 1;
+            B = 1;
+            C = 1;
+        }
+
+        public Triangle(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new ArgumentException("Error! The side of triangle should be more than zero.");
+
+            if (a > (b + c) || b > (a + c) || c > (a + b))
+                throw new ArithmeticException("Error! The side of triangle can't be greater than sum of other 2 sides.");
+
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public int A { get; }
+        public int B { get; }
+        public int C { get; }
+
+        public int GetPerimeter()
+        {
+            return A + B + C;
+        }
+
+        public double GetArea()
+        {
+            double p = (double)GetPerimeter() / 2;
+
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
 
-class Round
-{
-    public Round()
-    {
-        X = 0;
-        Y = 0;
-        Radius = 1;
-    }
 
-    public Round(int x, int y, int radius)
-    {
-        X = x;
-        Y = y;
-        Radius = radius;
-    }
-
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    private int _radius;
-    public int Radius
-    {
-        get { return _radius; }
-        set
-        {
-            if (value <= 0)
-                throw new ArgumentException("Radius should be more than zero", nameof(value));
-
-            _radius = value;
-        }
-    }
-
-    public double Length => 2 * Math.PI * Radius;
-    public double Area => Math.PI * Radius * Radius;
-
-}
 
 
 
