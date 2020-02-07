330070ef657a5ba0bc2374faa85b1e6347558147
Guzhikova <guzhikova.a@gmail.com>
2020-02-08 01:43:57 +0400

    Таск 9.4 - СДЕЛАН (исправлен баг с back)

--- Guzhikova.Task9/9.4_SIMPLE_GALLERY/scripts/main.js
+++ Guzhikova.Task9/9.4_SIMPLE_GALLERY/scripts/main.js
@@ -4,69 +4,88 @@ var pages = ['index.html', 'page2.html', 'page3.html', 'page4.html'];
 var countdownElement = document.getElementById('countdown');
 var actionBtn = document.getElementById('pause-run');
 var intervalId = {};
+var pageIndex = 0;
 
 var remainingTime = 3;
 
 
 function back() {
-     var backElement = document.getElementById('back'); 
-
-     if (getPageIndex() != 0) {
-          window.history.back();
-     } else {
-          document.location.href = pages[pages.length - 1];
-     }
+       
+     document.location.href = pages[pageIndexPreview()];
 }
 
 function getPageIndex() {
      return parseInt(localStorage.getItem("pageIndex")) || 0;
 }
 
-function pageIndex() {
+function setPageIndex(i) {
+     localStorage["pageIndex"] = i;
+}
 
-     var pageIndex = getPageIndex();
+function pageIndexNext() {
+
+     pageIndex = getPageIndex();
      pageIndex += 1;
+
+     setPageIndex(pageIndex);
+
+     console.log("next: " + getPageIndex());
+     return pageIndex;
+}
+
+function pageIndexPreview() {
+     pageIndex = getPageIndex();
+
+     pageIndex = (pageIndex != 0)
+          ? pageIndex - 1
+          : pages.length - 1;
 
      setPageIndex(pageIndex);
 
      return pageIndex;
 }
 
-function setPageIndex(i) {
-     localStorage["pageIndex"] = i;
+
+function pageTurning() {
+
+     pageIndex = getPageIndex();
+
+     if (pageIndex < pages.length - 1) {
+          runCountdown();
+     } else {
+          generateQuery();
+     }
+}
+
+function pauseCountdown() {
+
+     clearInterval(intervalId);
+     
+     actionBtn.textContent = "старт";
+     actionBtn.setAttribute('onclick', 'runCountdown ()');
 }
 
 function runCountdown() {
-console.log(getPageIndex());
 
-     actionBtn.textContent = "pause";
+     actionBtn.textContent = "пауза";
      actionBtn.setAttribute('onclick', 'pauseCountdown ()');
 
      intervalId = setInterval(() => {
+
           countdownElement.textContent = remainingTime + ' сек.';
+
           remainingTime--;
 
           if (remainingTime < 0) {
 
                clearInterval(intervalId);
 
-               var index = pageIndex();
-               if (index < pages.length) {
-
-                    document.location.href = pages[index];
-               } else {
-                    generateQuery();
-               }
-
+               document.location.href = pages[pageIndexNext()];
           }
      }, 1000);
 }
 
-function pauseCountdown() {
-     clearInterval(intervalId);
-     actionBtn.textContent = "run";
-     actionBtn.setAttribute('onclick', 'runCountdown ()');
-}
+
 
 function generateQuery() {
      var queryElement = document.getElementById('pageScrollingInfo');
