2a45b1ae81d70ec2d1c2db7fb67a27e00c3f3680
Guzhikova <guzhikova.a@gmail.com>
2020-02-07 04:20:29 +0400

    Таск 9.4 - сделан в черновом варианте

--- Guzhikova.Task9/9.4_SIMPLE_GALLERY/scripts/main.js
+++ Guzhikova.Task9/9.4_SIMPLE_GALLERY/scripts/main.js
@@ -1,51 +1,101 @@
 // &#9658;
 
-var pages = ['index.html','page2.html','page3.html', 'page4.html'];
-var index = getPageIndex ();
+var pages = ['index.html', 'page2.html', 'page3.html', 'page4.html'];
 
-function setPageIndex (i)
-{
-     console.log('onLoad = ' + getPageIndex ());
-     localStorage.setItem("pageIndex", i);  
+var countdownElement = document.getElementById('countdown');
+var actionBtn = document.getElementById('pause-run');
+var intervalId = {};
+
+var remainingTime = 3;
+
+
+//localStorage["pageIndex"] = 3; 
+
+function back() {
+     var backElement = document.getElementById('back'); 
+
+     if (getPageIndex() != 0) {
+          window.history.back();
+     } else {
+          document.location.href = pages[pages.length - 1];
+     }
+
+
 }
 
-function getPageIndex ()
-{
-     var result = localStorage.getItem("pageIndex");
-
-     console.log(result);
-
-     return (result != null) ? parseInt(result) : 0;
+function getPageIndex() {
+     return parseInt(localStorage.getItem("pageIndex")) || 0;
 }
 
-function allToRight() {
+function pageIndex() {
 
-     var countdownElement = document.getElementById('countdown');   
-   
-     var remainingTime = 5;
-     
-     
-     
+     var pageIndex = getPageIndex();
+     pageIndex += 1;
 
-     var intervalId = setInterval(() => {
-          
-          countdownElement.textContent =  remainingTime;
+     setPageIndex(pageIndex);
+
+     return pageIndex;
+}
+
+function setPageIndex(i) {
+     localStorage["pageIndex"] = i;
+}
+
+function runCountdown() {
+console.log(getPageIndex());
+
+     actionBtn.textContent = "pause";
+     actionBtn.setAttribute('onclick', 'pauseCountdown ()');
+
+     intervalId = setInterval(() => {
+          countdownElement.textContent = remainingTime + ' сек.';
           remainingTime--;
-console.log('index = ' + index);
-        if (remainingTime === 0 && index < pages.length) {
-  
-     
-         localStorage["pageIndex"] = index++;
 
-          document.location.href = pages[index];
-          
-          clearInterval(intervalId);
+          if (remainingTime < 0) {
+
+               clearInterval(intervalId);
+
+
+               var index = pageIndex();
+               if (index < pages.length) {
+
+                    document.location.href = pages[index];
+               } else {
+                    generateQuery();
+               }
+
           }
-        }, 1000);       
-
+     }, 1000);
 }
 
-function pause()
-{
+function pauseCountdown() {
      clearInterval(intervalId);
+     actionBtn.textContent = "run";
+     actionBtn.setAttribute('onclick', 'runCountdown ()');
 }
+
+function generateQuery() {
+     var queryElement = document.getElementById('pageScrollingInfo');
+     queryElement.textContent = "Прокрутка страниц завершена!";
+
+
+     var br = document.createElement('br');
+     queryElement.appendChild(br);
+
+     var aRepeat = document.createElement('a');
+     aRepeat.textContent = "Повторить прокрутку";
+     aRepeat.href = 'index.html';
+     queryElement.appendChild(aRepeat);
+
+     var aExit = document.createElement('a');
+     aExit.textContent = "Завершить просмотр";
+     aExit.href = '#';
+     aExit.onclick = 'window.close();'
+
+     queryElement.appendChild(aExit);
+
+}
+
+
+
+
