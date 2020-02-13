b3e5f4ee6c0bd086b575052acbacfc58293483e2
Guzhikova <guzhikova.a@gmail.com>
2020-02-13 19:23:27 +0400

    Начат таск 10

---
+++ Guzhikova.Task10/Guzhikova.Task10/WebManage.cs
@@ -0,0 +1,27 @@
+﻿using Guzhikova.Task6.BLLInterfaces;
+using Guzhikova.Task6.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Users.BLLInterfaces;
+using UsersAndAwards.Ioc;
+
+namespace Guzhikova.Task10
+{
+    public class WebManage
+    {
+        //private IUserLogic _userLogic = DependencyResolver.UserLogic;
+        //private IAwardLogic _awardLogic = DependencyResolver.AwardLogic;
+
+        public IUserLogic UserLogic => DependencyResolver.UserLogic;
+        public IAwardLogic AwardLogic => DependencyResolver.AwardLogic;
+
+        public void A()
+        {
+
+        }
+    }
+}
