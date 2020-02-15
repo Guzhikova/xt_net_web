6cc85f1cd97bf635e55a8629b79d0183bb9df7ca
Guzhikova <guzhikova.a@gmail.com>
2020-02-15 04:31:17 +0400

    Task 10.1: Delete function

--- Guzhikova.Task10/Guzhikova.Task10/WebManage.cs
+++ Guzhikova.Task10/Guzhikova.Task10/WebManage.cs
@@ -14,11 +14,63 @@ namespace Guzhikova.Task10
 {
     public class WebManage
     {
-        //private IUserLogic _userLogic = DependencyResolver.UserLogic;
-        //private IAwardLogic _awardLogic = DependencyResolver.AwardLogic;
+
+   User _currentUser = null;
+        Award _currentAward = null;
 
         public IUserLogic UserLogic => DependencyResolver.UserLogic;
         public IAwardLogic AwardLogic => DependencyResolver.AwardLogic;
 
-      }
+     
+
+        public void AddUser(string userName, string dateOfBirth, string awards)
+        {
+            DateTime date;
+            DateTime.TryParse(dateOfBirth, out date);
+
+            _currentUser = new User(userName, date);
+            UserLogic.Add(_currentUser);
+
+            UserLogic.SaveUsers();
+
+            if (awards != "")
+            {
+                int awardId;
+                string[] awardsId = awards.Split(',');
+
+                foreach (var item in awardsId)
+                {
+                    Int32.TryParse(item, out awardId);
+                    _currentAward = AwardLogic.GetById(awardId);
+                    _currentAward.UsersId.Add(_currentUser.Id);
+                }
+                AwardLogic.SaveAwards();
+            }
+        }
+
+        public void AddAward(string title)
+        {
+            _currentAward = new Award(title);
+            AwardLogic.Add(_currentAward);
+            AwardLogic.SaveAwards();
+        }
+
+        public void Delete(string entity, string currentId)
+        {
+            int id;
+            Int32.TryParse(currentId, out id);
+
+            if (entity == "user")
+            {
+                UserLogic.DeleteById(id);
+                UserLogic.SaveUsers();
+            }
+            else if(entity == "award")
+            {
+                AwardLogic.DeleteById(id);
+                AwardLogic.SaveAwards();
+            }
+        }
+
+    }
 }
