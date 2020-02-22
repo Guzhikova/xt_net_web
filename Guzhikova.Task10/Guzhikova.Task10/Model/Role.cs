eca21ca26c11312d232aeff07e30845290983faa
Guzhikova <guzhikova.a@gmail.com>
2020-02-23 01:52:09 +0400

    Task 10.4 - began

---
+++ Guzhikova.Task10/Guzhikova.Task10/Model/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Guzhikova.Task10.Model
+{
+    public class Role
+    {
+        public int ID{ get; set; }
+        public int Title { get; set; }
+    }
+}
---
+++ Guzhikova.Task10/Guzhikova.Task10/Model/WebUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Guzhikova.Task10.Model
+{
+    public class WebUser
+    {
+        private string _passwordstring;
+        public int ID { get; set; }
+        public string Login { get; set; }      
+
+        public  string Password
+        {
+            get { return _passwordstring; }
+            set { _passwordstring = value; }
+        }
+
+        public string Email { get; set; }
+
+    }
+}
